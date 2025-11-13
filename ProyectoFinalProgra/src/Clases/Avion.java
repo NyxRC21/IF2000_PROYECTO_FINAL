e594caea33df3b8f4bac2b23b06e73ca35aeb0f7
krisbelmadrigal-prog <krisbelmadrigal@gmail.com>
2025-11-13 00:21:37 -0600

    Avance

--- ProyectoFinalProgra/src/Clases/Avion.java
+++ ProyectoFinalProgra/src/Clases/Avion.java
@@ -9,5 +9,47 @@ package Clases;
  * @author Daniel Ríos
  */
 public class Avion {
+    private String codigo;
+    private boolean ejecutivo[];
+    private boolean economico[];
     
+    public Avion(String codigo){
+        this.ejecutivo = ejecutivo;
+        this.economico = economico;
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public boolean[] getEjecutivo() {
+        return ejecutivo;
+    }
+
+    public boolean[] getEconomico() {
+        return economico;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEjecutivo(boolean[] ejecutivo) {
+        this.ejecutivo = ejecutivo;
+    }
+
+    public void setEconomico(boolean[] economico) {
+        this.economico = economico;
+    }
+    
+    public String toString (){
+        return "Codigo{" +
+                "ejecutivo='" + ejecutivo + '\'' +
+                ", economico='" + economico + '\'' +
+                '}';
+    }
 }
+    
+    
+
--- ProyectoFinalProgra/src/Clases/Factura.java
+++ ProyectoFinalProgra/src/Clases/Factura.java
@@ -6,8 +6,70 @@ package Clases;
 
 /**
  *
- * @author Daniel Ríos
+ * @author Daniel Ríos y Crisbel Madrigal
  */
 public class Factura {
+
+    private String id;
+    private Tiquete tiquete;
+    private double subtotal;
+    private double iva;
+    private double total;
+
+    public Factura(String id, Tiquete tiquete, double subtotal, double iva, double total) {
+        this.id = id;
+        this.tiquete = tiquete;
+        this.subtotal = subtotal;
+        this.iva = iva;
+        this.total = total;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Tiquete getTiquete() {
+        return tiquete;
+    }
+
+    public void setTiquete(Tiquete tiquete) {
+        this.tiquete = tiquete;
+    }
+
+    public double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    @Override
+    public String toString() {
+        return "Factura{" + "id=" + id + ", tiquete=" + tiquete + ", subtotal=" + subtotal + ", iva=" + iva + ", total=" + total + '}';
+    }
     
-}
+    
+                
+    }
+
