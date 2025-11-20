8dc1bc7da49eb7cfb23266f22376848cdd5a6bc3
krisbelmadrigal-prog <krisbelmadrigal@gmail.com>
2025-11-20 02:20:09 -0600

    últimos detalles del proyecto

--- ProyectoFinalProgra/src/UI/InterfazBoleteria.java
+++ ProyectoFinalProgra/src/UI/InterfazBoleteria.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package UI;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
